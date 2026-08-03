babb66f6f172cf26f2585b55a3f8e4b88b3068d3
hajjiyusuf1445-byte <hajjiyusuf1445@gmail.com>
2026-08-03 11:39:34 +0700

    Fix HTTPS URL dan routing Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Struk from './Struk'
 import './Struk.css'
 import './App.css'
 
-const API_URL = 'http://projectgampangtoko-production.up.railway.app'
+const API_URL = 'https://projectgampangtoko-production.up.railway.app'
 
 function App() {
   const [cart, setCart] = useState([])
